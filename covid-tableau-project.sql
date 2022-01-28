0ff0e359fe8fa281fea14e9bd3627bde9b45757d
ange-lousi <lousiangenita@gmail.com>
2022-01-28 22:01:50 +1300

    update sql file

--- covid-tableau-project.sql
+++ covid-tableau-project.sql
@@ -2,12 +2,12 @@
 
 
 -- 1.  Global death percentage
-Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
-From portfolioProject..[covid-deaths]
+SELECT SUM(new_cases) AS total_cases, SUM(cast(new_deaths AS int)) AS total_deaths, SUM(cast(new_deaths AS int))/SUM(New_Cases)*100 AS DeathPercentage
+FROM portfolioProject..[covid-deaths]
 --Where location like '%states%'
-where continent is not null 
+WHERE continent is not NULL 
 --Group By date
-order by 1,2
+ORDER BY 1,2
 
 -- 2. Showing the continents w/ the highest death count
 SELECT continent, SUM(CAST(new_deaths AS int)) AS totalDeathCount
@@ -18,16 +18,16 @@ GROUP BY continent
 ORDER BY totalDeathCount DESC
 
 -- 3. Percent of world population infected
-Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
-From portfolioProject..[covid-deaths]
-Group by Location, Population
-order by PercentPopulationInfected desc
+SELECT Location, Population, MAX(total_cases) AS HighestInfectionCount,  Max((total_cases/population))*100 AS PercentPopulationInfected
+FROM portfolioProject..[covid-deaths]
+GROUP BY Location, Population
+ORDER BY PercentPopulationInfected DESC
 
 -- 4. HighestInfectionCount
-Select Location, Population, date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
-From portfolioProject..[covid-deaths]
+SELECT Location, Population, date, MAX(total_cases) AS HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+FROM portfolioProject..[covid-deaths]
 --Where location like '%states%'
-Group by Location, Population, date
-order by PercentPopulationInfected desc
+GROUP BY Location, Population, date
+ORDER BY PercentPopulationInfected DESC
  
 
